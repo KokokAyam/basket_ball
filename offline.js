d29c979e112c67abf6bb0eede2a6968918107125
KokokAyam <75647574+KokokAyam@users.noreply.github.com>
2021-01-10 07:20:43 +0700

    initial commit

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1610237976,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/football-sheet0.png",
+		"images/goal-sheet0.png",
+		"images/goal-sheet1.png",
+		"images/nethit-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/deck-sheet0.png",
+		"images/underout-sheet0.png",
+		"images/hp-sheet0.png",
+		"images/go-sheet0.png",
+		"images/wallout-sheet0.png",
+		"images/playbtn-sheet0.png",
+		"media/basket.ogg",
+		"media/basketball.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
